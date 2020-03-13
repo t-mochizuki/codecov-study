31a62883bdab115ebdf2ca4ef32541b3878bbd25
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-03-13 23:30:56 +0900

    fix main.py
    I run the following command line to upload coverage reports.
    
    ```
    cd projectA
    pipenv run pytest --cov=./../ --cov-report=xml
    cd ..
    cd projectB
    pipenv run pytest --cov=./../ --cov-report=xml
    cd ..
    bash <(curl https://codecov.io/bash) -t <token>
    ```

--- projectB/src/main.py
+++ projectB/src/main.py
@@ -4,5 +4,5 @@ def subtraction(a, b):
 if __name__ == "__main__":
     import sys
     args = sys.argv
-    if len(args) == 2:
-        print(hello(args[1], args[2]))
+    if len(args) > 2:
+        print(subtraction(int(args[1]), int(args[2])))
