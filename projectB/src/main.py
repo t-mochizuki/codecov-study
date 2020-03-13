79dbb979cd9467ca3301d2f7032c4564c72dc206
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-03-13 23:23:30 +0900

    Add projectB
    I ran `bash <(curl -s https://codecov.io/bash)` in root directory without editing.

---
+++ projectB/src/main.py
@@ -0,0 +1,8 @@
+def subtraction(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    if len(args) == 2:
+        print(hello(args[1], args[2]))
---
+++ projectB/tests/test_main.py
@@ -0,0 +1,4 @@
+from main import subtraction
+
+def test_subtraction():
+    assert subtraction(321, 123) == 321 - 123
