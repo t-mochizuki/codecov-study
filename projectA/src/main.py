eda9a1efbaeb8c149825b8c79d566efc8c9fa5d4
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-03-13 23:02:37 +0900

    I ran bash codecov command in root directory with editing

--- projectA/src/main.py
+++ projectA/src/main.py
@@ -1,5 +1,5 @@
 def hello(name):
-    return "helo, " + name
+    return "hello, " + name
 
 if __name__ == "__main__":
     import sys
--- projectA/tests/test_main.py
+++ projectA/tests/test_main.py
@@ -1,4 +1,4 @@
 from main import hello
 
 def test_hello():
-    assert hello("taka") == "helo, taka"
+    assert hello("taka") == "hello, taka"
